662720dec81fc1348ec56ae440aeeb1a23fa3245
XSans0 <xsansdroid@gmail.com>
2024-01-24 22:44:40 +0700

    build.sh: Fix ShellCheck `SC2034` to get binutils version
    Problematic code SC2034: https://www.shellcheck.net/wiki/SC2034.
    
    Signed-off-by: XSans0 <xsansdroid@gmail.com>

--- build.sh
+++ build.sh
@@ -63,26 +63,30 @@ done
 git config --global user.name "XSans0"
 git config --global user.email "xsansdroid@gmail.com"
 
-# Release Info
+# Get Clang Info
 pushd "$HOME_DIR"/src/llvm-project || exit
 llvm_commit="$(git rev-parse HEAD)"
 short_llvm_commit="$(cut -c-8 <<<"$llvm_commit")"
 popd || exit
 llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
-binutils_ver="$(ls | grep "$HOME_DIR/src/^binutils-" | sed "s/binutils-//g")"
 clang_version="$("$HOME_DIR"/install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 build_date="$(TZ=Asia/Jakarta date +"%Y-%m-%d")"
 tags="WeebX-Clang-$clang_version-release"
 file="WeebX-Clang-$clang_version.tar.gz"
 clang_link="https://github.com/XSans0/WeebX-Clang/releases/download/$tags/$file"
 
-# Get simple info
+# Get binutils version
+binutils_version=$(grep "LATEST_BINUTILS_RELEASE" build-binutils.py)
+binutils_version=$(echo "$binutils_version" | grep -oP '\(\s*\K\d+,\s*\d+,\s*\d+' | tr -d ' ')
+binutils_version=$(echo "$binutils_version" | tr ',' '.')
+
+# Create simple info
 pushd "$HOME_DIR"/install || exit
 {
     echo "# Quick Info
 * Build Date : $build_date
 * Clang Version : $clang_version
-* Binutils Version : $binutils_ver
+* Binutils Version : $binutils_version
 * Compiled Based : $llvm_commit_url"
 } >>README.md
 tar -czvf ../"$file" .
